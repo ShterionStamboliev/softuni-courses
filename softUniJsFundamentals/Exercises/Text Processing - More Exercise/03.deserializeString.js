8afbe360507b38a1454f7cd06cda56af36bf20ed
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-07-31 23:51:44 +0300

    Added new task in TextProcessing - MoreExercise.

--- softUniJsFundamentals/Exercises/Text Processing - More Exercise/03.deserializeString.js
+++ softUniJsFundamentals/Exercises/Text Processing - More Exercise/03.deserializeString.js
@@ -1,26 +1,38 @@
 function deserializeString(input) {
-    let box = [];
-
-    for (let i = 0; i < input.length; i++) {
-        let currentInput = input[i];
-        if (currentInput === 'end') {
-            break;
-        }
-        let [currentChar, currentIndex] = currentInput.split(':');
-        currentIndex = currentIndex.split('/').map(Number).join(' ');
-        box.push(currentIndex);
-        box.sort((a, b) => a - b);
-
-        // for (let j = 0; j < box.length; j++) {
-        //     if (j % 2 === 0) {
-        //         box[j] = currentChar;
-        //     } 
-        //     if (j % 2 !== 0) {
-        //         box[j] = currentChar;
-        //     }
-        // }
-        
+  let numBox = [];
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === "end") {
+      break;
     }
-    console.log(box.join(''));
+    let [currentChar, currentIndexes] = input[i].split(":");
+    currentChar = currentChar;
+    currentIndexes = currentIndexes.split("/").map(Number);
+    numBox = numBox.concat(currentIndexes).sort((a, b) => a - b);
+  }
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === "end") {
+      break;
+    }
+    let [char, index] = input[i].split(":");
+    index = index.split("/").map(Number);
+    let charIndex = [];
+    charIndex.push(char);
+    charIndex = charIndex.concat(index);
+    let ch = charIndex.shift();
+    for (let j = 0; j < charIndex.length; j++) {
+        numBox.includes(charIndex[j]) ? numBox.splice(charIndex[j], 1, ch) : ''
+    }
+  }
+  console.log(numBox.join(""));
 }
-deserializeString(['a:0/2/4/6','b:1/3/5','end']);
+deserializeString([
+  "a:0/3/5/11",
+  "v:1/4",
+  "j:2",
+  "m:6/9/15",
+  "s:7/13",
+  "d:8/14",
+  "c:10",
+  "l:12",
+  "end",
+]);
