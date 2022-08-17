618a9857b22aa2038932e8d91058e33f04836c67
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-17 19:09:35 +0300

    Code refactoring.

--- softuni-advanced/arrays-and-nested-arrays-Lab/08.processOddPositions.js
+++ softuni-advanced/arrays-and-nested-arrays-Lab/08.processOddPositions.js
@@ -1,5 +1,12 @@
 function processOddPositions(input) {
-    return input.filter((el, index) => index % 2 === 1).map((a, b) => a * 2).reverse().join(' ');
+    // return input.filter((el, index) => index % 2 === 1).map((a) => a * 2).reverse().join(' ');
+    const result = input.reduce((sum, val, index) => {
+        if (index % 2 === 1) {
+            sum.push(val * 2);
+        }
+        return sum;
+    }, []);
+    console.log(result.reverse().join(' '));
 }
 processOddPositions([10, 15, 20, 25]);
 processOddPositions([3, 0, 10, 4, 7, 3]);
