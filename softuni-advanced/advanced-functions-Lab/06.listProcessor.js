0794183d2750b58f96627711c557c38e3978e842
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-26 15:08:45 +0300

    Code refactoring.

--- softuni-advanced/advanced-functions-Lab/06.listProcessor.js
+++ softuni-advanced/advanced-functions-Lab/06.listProcessor.js
@@ -1,19 +1,21 @@
 function listProcessor(commands) {
-  const process = (function () {
-      let listOfNames = [];
-      const add = str => listOfNames.push(str);
-      const remove = text =>
-      (listOfNames = listOfNames.filter(name => name !== text));
-      const print = () => console.log(listOfNames.join(","));
-      return {
-        add,
-        remove,
-        print
-      }
-  })();
-  commands.forEach(command => {
-    const [queCommand, orderCommand] = command.split(" ");
-    process[queCommand](orderCommand);
-  });
+
+    
+    const process = (() => {
+        let listOfNames = [];
+        const add = str => listOfNames.push(str);
+        const remove = text =>
+        (listOfNames = listOfNames.filter(name => name !== text));
+        const print = () => console.log(listOfNames.join(","));
+        return {
+            add,
+            remove,
+            print,
+        }
+    })();
+    commands.forEach(command => {
+        let [queCommand, action] = command.split(" ");
+        process[queCommand](action)
+    });
 }
-listProcessor(["add hello", "add again", "remove hello", "add again", "print"]);
+listProcessor(['add pesho', 'add george', 'add peter', 'remove peter','print']);
