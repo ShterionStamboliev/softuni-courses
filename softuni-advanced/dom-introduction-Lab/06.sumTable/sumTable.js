2af3864995988bbfdc73d505441add432a33b4d3
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-29 17:18:33 +0300

    .

--- softuni-advanced/dom-introduction-Lab/06.sumTable/sumTable.js
+++ softuni-advanced/dom-introduction-Lab/06.sumTable/sumTable.js
@@ -1,3 +1,9 @@
 function sumTable() {
-
+    const nums = Array.from(document.querySelectorAll('tr')).slice(1, -1);
+    let sum = 0;
+    for (const num of nums) {
+        const numVal = Number(num.lastElementChild.textContent);
+        sum += numVal;
+    }
+    document.getElementById('sum').textContent = sum;
 }
