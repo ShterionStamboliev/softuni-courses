c42c816e753b6c6a41ae233e8fe699f7fdd914e3
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-29 15:51:16 +0300

    .

--- softuni-advanced/dom-introduction-Lab/03.sumNumbers/sumNumbers.js
+++ softuni-advanced/dom-introduction-Lab/03.sumNumbers/sumNumbers.js
@@ -1,3 +1,5 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const totalSum = Number(document.getElementById('num1').value) + 
+    Number(document.getElementById('num2').value);
+    document.getElementById('sum').value = totalSum;
 }
