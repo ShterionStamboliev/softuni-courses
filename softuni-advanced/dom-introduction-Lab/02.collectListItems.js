a661866988411a18f1790943695c677e1c45a824
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-29 15:31:05 +0300

    Delete softuni-advanced/dom-introduction-Lab directory

--- softuni-advanced/dom-introduction-Lab/02.collectListItems.js
+++
@@ -1,5 +0,0 @@
-function extractText() {
-    const getItems = document.querySelectorAll("ul#items li");
-    const parsed = Array.from(getItems).map(text => text.textContent).join('\n');
-    document.getElementById('result').value = parsed;
-}
