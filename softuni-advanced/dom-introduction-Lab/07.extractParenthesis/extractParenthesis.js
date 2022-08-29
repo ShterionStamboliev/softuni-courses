ecd81b80c0aff169abb5a17e90cd588c50935b95
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-29 19:17:17 +0300

    .

--- softuni-advanced/dom-introduction-Lab/07.extractParenthesis/extractParenthesis.js
+++ softuni-advanced/dom-introduction-Lab/07.extractParenthesis/extractParenthesis.js
@@ -1,3 +1,13 @@
 function extract(content) {
+    const text = document.getElementById(content).textContent;
+    const regMatch = /\((.+?)\)/gm
+    let match = regMatch.exec(text);
+    let res = '';
 
+    while (match) {
+        res += match[1] + '; ';
+        match = regMatch.exec(text);
+    }
+    return res;
 }
+let text = extract("content");
