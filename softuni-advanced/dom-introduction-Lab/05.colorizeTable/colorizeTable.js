a9e1a130b50f10753787f479a901f924cbbb7a1b
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-29 16:28:34 +0300

    .

--- softuni-advanced/dom-introduction-Lab/05.colorizeTable/colorizeTable.js
+++ softuni-advanced/dom-introduction-Lab/05.colorizeTable/colorizeTable.js
@@ -1,3 +1,6 @@
 function colorize() {
-    // TODO
+    let tableRows = Array.from(document.querySelectorAll('tr'));
+    for (let i = 1; i < tableRows.length; i+=2) {
+        tableRows[i].style.background = 'teal';
+    }
 }
