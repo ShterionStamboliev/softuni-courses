e48e91696d046ca3905507f57621f6886b85df19
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-30 15:09:27 +0300

    .

--- softuni-advanced/dom-introduction-Exercise/04.searchInList/search.js
+++ softuni-advanced/dom-introduction-Exercise/04.searchInList/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+  let townNames = document.querySelectorAll("ul#towns li");
+  let resultField = document.getElementById("result");
+  let searchField = document.getElementById("searchText").value;
+  let matchesCount = 0;
+
+  for (const town of townNames) {
+    let str = town.textContent;
+    if (str.match(searchField)) {
+      matchesCount++;
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+    } else {
+      town.style.fontWeight = "normal";
+      town.style.textDecoration = "none";
+    }
+  }
+  resultField.textContent = `${matchesCount} matches found`;
 }
