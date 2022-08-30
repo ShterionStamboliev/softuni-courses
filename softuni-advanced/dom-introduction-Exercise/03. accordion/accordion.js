3f970cf2290690fe07c488710acb58a3043a8d91
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-30 14:12:46 +0300

    .

--- softuni-advanced/dom-introduction-Exercise/03. accordion/accordion.js
+++ softuni-advanced/dom-introduction-Exercise/03. accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let showText = document.getElementById('extra');
+    let clickButton = document.getElementsByClassName('button')[0];
+
+    if (clickButton.textContent === 'More') {
+        showText.style.display = 'block';
+        clickButton.textContent = 'Less';
+    } else {
+        showText.style.display = 'none';
+        clickButton.textContent = 'More';
+    }
 }
