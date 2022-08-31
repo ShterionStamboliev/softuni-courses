e31510d94426639c2914f38f78c0119f9e632842
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-31 16:02:18 +0300

    .

--- softuni-advanced/dom-introduction-Exercise/08.generateReport/report.js
+++ softuni-advanced/dom-introduction-Exercise/08.generateReport/report.js
@@ -1,3 +1,27 @@
 function generateReport() {
-    //TODO
+    let tableHead = Array.from(document.getElementsByTagName('input'))
+    let employeesData = Array.from(document.querySelectorAll('tbody tr'));
+    let textOutput = document.getElementById('output');
+    let checkBox = [];
+    let allEmployees = [];
+
+
+    tableHead.forEach((check, col) => {
+        if (check.checked === true) {
+            checkBox.push(col);
+        }
+    });
+
+    employeesData.forEach((_, i) => {
+        let arr = Array.from(employeesData[i].children);
+        let employeeInfo = {};
+
+        checkBox.forEach((_, j) => {
+            let columnName = tableHead[checkBox[j]].name.toLowerCase();
+            let columnValue = arr[checkBox[j]].textContent;
+            employeeInfo[columnName] = columnValue;
+        });
+        allEmployees.push(employeeInfo);
+    });
+    textOutput.textContent = JSON.stringify(allEmployees);
 }
