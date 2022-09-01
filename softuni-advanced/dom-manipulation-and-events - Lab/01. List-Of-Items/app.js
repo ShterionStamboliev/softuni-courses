90f22ac6d78c164d05acbd2f5e025900b0274f97
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-01 22:49:47 +0300

    .

--- softuni-advanced/dom-manipulation-and-events - Lab/01. List-Of-Items/app.js
+++ softuni-advanced/dom-manipulation-and-events - Lab/01. List-Of-Items/app.js
@@ -1,3 +1,7 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputField = document.getElementById('newItemText').value;
+    const newElement = document.createElement('li')
+    newElement.textContent = inputField;
+    document.getElementById('items').appendChild(newElement);
+    document.getElementById('newItemText').value = '';
 }
