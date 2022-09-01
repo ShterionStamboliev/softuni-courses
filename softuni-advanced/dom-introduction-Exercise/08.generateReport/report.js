8facfc01259f8430beedd9f42dda8c58c512c331
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-01 21:51:03 +0300

    Update report.js

--- softuni-advanced/dom-introduction-Exercise/08.generateReport/report.js
+++ softuni-advanced/dom-introduction-Exercise/08.generateReport/report.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function generateReport() {
     let tableHead = Array.from(document.getElementsByTagName('input'))
     let employeesData = Array.from(document.querySelectorAll('tbody tr'));
@@ -26,32 +25,3 @@ function generateReport() {
     });
     textOutput.textContent = JSON.stringify(allEmployees);
 }
-=======
-function generateReport() {
-    let tableHead = Array.from(document.getElementsByTagName('input'))
-    let employeesData = Array.from(document.querySelectorAll('tbody tr'));
-    let textOutput = document.getElementById('output');
-    let checkBox = [];
-    let allEmployees = [];
-
-
-    tableHead.forEach((check, col) => {
-        if (check.checked === true) {
-            checkBox.push(col);
-        }
-    });
-
-    employeesData.forEach((_, i) => {
-        let arr = Array.from(employeesData[i].children);
-        let employeeInfo = {};
-
-        checkBox.forEach((_, j) => {
-            let columnName = tableHead[checkBox[j]].name.toLowerCase();
-            let columnValue = arr[checkBox[j]].textContent;
-            employeeInfo[columnName] = columnValue;
-        });
-        allEmployees.push(employeeInfo);
-    });
-    textOutput.textContent = JSON.stringify(allEmployees);
-}
->>>>>>> 8e62e8dd859c43444a795f5030eee3f00e03ad7b
