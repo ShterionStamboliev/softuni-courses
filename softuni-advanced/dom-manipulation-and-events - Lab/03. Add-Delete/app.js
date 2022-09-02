f14a42cbb4dccb82e9d31ad4f5d7f36ce59cd36d
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-02 18:18:14 +0300

    .

--- softuni-advanced/dom-manipulation-and-events - Lab/03. Add-Delete/app.js
+++ softuni-advanced/dom-manipulation-and-events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,17 @@
 function addItem() {
-    //TODO...
+    const inputField = document.getElementById('newItemText').value;
+    const newElement = document.createElement('li');
+    newElement.textContent = inputField;
+
+    const deleteButton = document.createElement('a');
+    deleteButton.textContent = '[Delete]';
+    deleteButton.href = '#';
+    newElement.appendChild(deleteButton);
+    document.getElementById('items').appendChild(newElement);
+    document.getElementById('newItemText').value = '';
+    deleteButton.addEventListener('click', function() {
+        newElement.remove();
+    });
 }
+
+
