4018947003a70ff9fb5002c79e561ce2ee885eee
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-02 20:38:41 +0300

    .

--- softuni-advanced/dom-manipulation-and-events - Lab/05. Highlight-Active/app.js
+++ softuni-advanced/dom-manipulation-and-events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,13 @@
 function focused() {
-    console.log('TODO:...');
+    [...document.querySelectorAll('input')]
+    .forEach(inputField => {
+        inputField.addEventListener('focus', targetFocus),
+        inputField.addEventListener('blur', targetBlur)
+    });
+}
+const targetFocus = eventQueue => {
+    eventQueue.target.parentElement.className = 'focused';
+}
+const targetBlur = eventQueue => {
+    eventQueue.target.parentElement.className = '';
 }
