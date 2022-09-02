62ece6eaa2473cbf5262f2a45eee5a1d486737a1
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-02 19:50:00 +0300

    .

--- softuni-advanced/dom-manipulation-and-events - Lab/04. Mouse-Gradient/app.js
+++ softuni-advanced/dom-manipulation-and-events - Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  const gradientElement = document.getElementById("gradient");
+  gradientElement.addEventListener("mousemove", gradientHover);
+  gradientElement.addEventListener("mouseleave", gradientOut);
+  function gradientHover(event) {
+    const percentage =
+      Math.floor((event.offsetX / gradientElement.clientWidth) * 100) + "%";
+    document.getElementById("result").textContent = percentage;
+  }
+  function gradientOut() {
+    document.getElementById("result").textContent = "";
+  }
 }
