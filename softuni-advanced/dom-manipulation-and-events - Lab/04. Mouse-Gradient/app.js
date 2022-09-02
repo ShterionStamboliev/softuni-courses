dfcec32f0b7e19018b4a3d86999dfa170b20a5cb
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-02 23:10:32 +0300

    .

--- softuni-advanced/dom-manipulation-and-events - Lab/04. Mouse-Gradient/app.js
+++ softuni-advanced/dom-manipulation-and-events - Lab/04. Mouse-Gradient/app.js
@@ -1,13 +1,15 @@
 function attachGradientEvents() {
-  const gradientElement = document.getElementById("gradient");
-  gradientElement.addEventListener("mousemove", gradientHover);
-  gradientElement.addEventListener("mouseleave", gradientOut);
-  function gradientHover(event) {
-    const percentage =
-      Math.floor((event.offsetX / gradientElement.clientWidth) * 100) + "%";
-    document.getElementById("result").textContent = percentage;
-  }
-  function gradientOut() {
-    document.getElementById("result").textContent = "";
-  }
+  document.getElementById("gradient").addEventListener("mousemove", gradientHover);
+  document.getElementById("gradient").addEventListener("mouseleave", gradientOut);
 }
+const gradientHover = (event) => {
+  const percentage =
+    Math.floor((event.offsetX / document.getElementById("gradient").clientWidth) * 100) + "%";
+  document.getElementById("result").textContent = percentage;
+}
+const gradientOut = () => 
+  document.getElementById("result").textContent = "";
+
+  window.onload = async () => {
+    await attachGradientEvents();
+  }
