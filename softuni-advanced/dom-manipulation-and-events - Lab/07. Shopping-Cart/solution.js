bd915a2c52808d76f85ff92c9590cc4f97902d95
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-03 23:56:45 +0300

    .

--- softuni-advanced/dom-manipulation-and-events - Lab/07. Shopping-Cart/solution.js
+++ softuni-advanced/dom-manipulation-and-events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-   //TODO...
+  const textArea = document.querySelector("textarea");
+  const addButton = [...document.querySelectorAll("div[class=product-add]")];
+  addButton.forEach((button) => button.addEventListener("click", addProducts));
+  document.getElementsByClassName('checkout')[0].addEventListener('click', checkOut)
+  const shoppingCart = [];
+
+  function addProducts(btn) {
+    if ((btn.target = "BUTTON")) {
+      const product = btn.target.parentElement.parentElement;
+      const productName = product.querySelector(".product-title").textContent;
+      const productPrice = Number(product.querySelector(".product-line-price").textContent);
+      shoppingCart.push({
+        productName,
+        productPrice,
+      });
+      textArea.value += `Added ${productName} for ${productPrice} to the cart.\n`;
+    }
+  }
+  function checkOut() {
+   let finalProducts = new Set();
+   shoppingCart.forEach(product => finalProducts.add(product.productName));
+   let finalSum = shoppingCart.reduce((a, b) => a + b.productPrice, 0).toFixed(2);
+   finalProducts.forEach(prod => console.log(`You bought ${prod}`))
+  }
+//   textArea.value = ''
 }
