ba09ba77c2c6705c3a42be08d8d57666275d890e
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-04 23:33:11 +0300

    .

--- softuni-advanced/dom-manipulation-and-events-Exercise/04. Fill-Dropdown/dropdown.js
+++ softuni-advanced/dom-manipulation-and-events-Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+  const itemText = document.getElementById("newItemText").value;
+  const itemValue = document.getElementById("newItemValue").value;
+  const selectMenu = document.getElementById("menu");
+  const options = document.createElement("option");
+  
+  options.textContent = itemText;
+  options.value = itemValue;
+  selectMenu.appendChild(options);
+  document.getElementById("newItemText").value = "";
+  document.getElementById("newItemValue").value = "";
 }
