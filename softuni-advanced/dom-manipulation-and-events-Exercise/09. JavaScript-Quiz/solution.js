104a92595022fc7f1608697c72dd155931ef6c78
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-07 16:48:12 +0300

    .

--- softuni-advanced/dom-manipulation-and-events-Exercise/09. JavaScript-Quiz/solution.js
+++ softuni-advanced/dom-manipulation-and-events-Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-  //TODO...
+  const answers = [];
+  let questionsIndex = 0;
+  let rightAnswersCount = 0;
+  const questions = document.getElementsByTagName('section');
+
+  [...document.querySelectorAll("ul li div p")].forEach(q => {
+    if (q.textContent === 'onclick' || q.textContent === 'JSON.stringify()' || q.textContent === 'A programming API for HTML and XML documents') {
+      answers.push(q.textContent.trim());
+    } 
+});
+
+[...document.querySelectorAll('.quiz-answer')].forEach(q => {
+  q.addEventListener('click', event => {
+      if (answers.includes(event.target.textContent)) {
+        rightAnswersCount++;
+      }
+      questions[questionsIndex].style.display = 'none';
+      questionsIndex++;
+      if (questionsIndex !== 3) {
+        questions[questionsIndex].style.display = 'block';
+      } else {
+        results(rightAnswersCount);
+      }
+  });
+});
+
+  const results = rightAnswersCount => {
+  let res = '';
+    document.querySelector('#results').style.display = 'block'
+    rightAnswersCount === 3 ? res = 'You are recognized as top JavaScript fan!' : res = `You have ${rightAnswersCount} right answers`;
+    document.querySelector('ul li h1').textContent = res;
+  }
 }
