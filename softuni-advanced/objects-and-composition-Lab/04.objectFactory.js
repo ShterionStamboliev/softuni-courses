f01eb0aa12725e36c5552cd0ef5d17c966f8b2c6
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-14 00:35:28 +0300

    Update 04.objectFactory.js

--- softuni-advanced/objects-and-composition-Lab/04.objectFactory.js
+++ softuni-advanced/objects-and-composition-Lab/04.objectFactory.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function factory(library, orders) {
 
   let box = [];
@@ -46,62 +45,3 @@ const library = {
 
   const products = factory(library, orders);
   console.log(products);
-
-
-
-
-
-=======
-function factory(library, orders) {
-
-  let box = [];
-
-  for (const info of orders) {
-    const printer = {...info.template}
-
-    for (const part of info.parts) {
-      printer[part] = library[part];
-    }
-    box.push(printer);
-  }
-  return box;
-}
-
-const library = {
-    print: function () {
-      console.log(`${this.name} is printing a page`);
-    },
-    scan: function () {
-      console.log(`${this.name} is scanning a document`);
-    },
-    play: function (artist, track) {
-      console.log(`${this.name} is playing '${track}' by ${artist}`);
-    },
-  };
-  const orders = [
-    {
-      template: { name: 'ACME Printer'},
-      parts: ['print']      
-    },
-    {
-      template: { name: 'Initech Scanner'},
-      parts: ['scan']      
-    },
-    {
-      template: { name: 'ComTron Copier'},
-      parts: ['scan', 'print']      
-    },
-    {
-      template: { name: 'BoomBox Stereo'},
-      parts: ['play']      
-    }
-  ];
-
-  const products = factory(library, orders);
-  console.log(products);
-
-
-
-
-
->>>>>>> 2629d4553db1ea799886b9a6c5ea625c874a2410
