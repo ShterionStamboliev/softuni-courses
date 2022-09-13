22de21c941f709d26bf2edca8369742c215be4f7
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-13 15:11:42 +0300

    .

--- softuni-advanced/arrays-and-nested-arrays-Exercise/03.addAndRemoveElements.js
+++ softuni-advanced/arrays-and-nested-arrays-Exercise/03.addAndRemoveElements.js
@@ -3,19 +3,10 @@ function addAndRemoveElements(input) {
   let count = 0;
 
   for (const command of input) {
-    if (command === "add") {
-      count++;
-      arr.push(count);
-    } else if (command === "remove") {
-      count++;
-      arr.pop(count);
-    }
+    command === 'add' ? count++ : arr.pop(count);
+    command === 'remove' ? count++ : arr.push(count);
   }
-  if (arr.length === 0) {
-    console.log("Empty");
-  } else {
-    console.log(arr.join('\n'));
-  }
+  arr.length === 0 ? console.log('Empty') : console.log(arr.join('\n'));
 }
 addAndRemoveElements(["add", "add", "add", "add"]);
 addAndRemoveElements(["add", "add", "remove", "add", "add"]);
