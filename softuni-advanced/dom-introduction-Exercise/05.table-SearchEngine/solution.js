a805674f761583588f33a5ae9a0478272864dd6e
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-14 14:41:52 +0300

    .

--- softuni-advanced/dom-introduction-Exercise/05.table-SearchEngine/solution.js
+++ softuni-advanced/dom-introduction-Exercise/05.table-SearchEngine/solution.js
@@ -1,10 +1,10 @@
 function solve() {
   document.querySelector("#searchBtn").addEventListener("click", onClick);
-  const tableContent = document.querySelectorAll("tbody tr");
+  const tableContent = [...document.querySelectorAll("tbody tr")];
   let searchField = document.getElementById("searchField");
 
   function onClick() {
-    tableContent.forEach((el) => {
+    tableContent.map((el) => {
       if (el.innerHTML.includes(searchField.value)) {
         el.className = "select";
       } else {
