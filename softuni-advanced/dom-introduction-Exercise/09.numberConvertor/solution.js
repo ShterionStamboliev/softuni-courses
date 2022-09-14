43277e4053c8d6456e4cf9f18479c2d803de8265
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-14 16:52:19 +0300

    .

--- softuni-advanced/dom-introduction-Exercise/09.numberConvertor/solution.js
+++ softuni-advanced/dom-introduction-Exercise/09.numberConvertor/solution.js
@@ -1,4 +1,27 @@
 function solve() {
+    const inputField = document.getElementById('input');
+    const hexa = elements('option', 'selected', 'hexadecimal', 'Hexadecimal');
+    const binary = elements('option', 'selected', 'binary', 'Binary');
+    const selectMenu = document.getElementById('selectMenuTo');
+    const result = document.getElementById('result');
+    selectMenu.appendChild(hexa);
+    selectMenu.appendChild(binary);
 
-    //TODO...
+    document.getElementsByTagName('button')[0].addEventListener('click', () => {
+        selectMenu.value === 'binary' 
+        ? result.value = Number(inputField.value).toString(2) 
+        : result.value = Number(inputField.value).toString(16).toUpperCase();
+    });
+    
+    function elements(type, classType, val, text) {
+        const element = document.createElement(type);
+        element.textContent = text;
+        if (classType) {
+            element.setAttribute('selected', '');
+        }
+        if (val) {
+            element.setAttribute('value', val)
+        }
+        return element;
+    };
 }
