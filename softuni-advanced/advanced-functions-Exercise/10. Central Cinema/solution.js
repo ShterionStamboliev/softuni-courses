24706101dd8a859feaddad289658428f1b569004
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-18 23:54:18 +0300

    .

--- softuni-advanced/advanced-functions-Exercise/10. Central Cinema/solution.js
+++ softuni-advanced/advanced-functions-Exercise/10. Central Cinema/solution.js
@@ -1,83 +1,78 @@
 function solve() {
- 
-    const name = document.querySelector('#container input:nth-of-type(1)');
-    const hall = document.querySelector('#container input:nth-of-type(2)');
-    const ticketPrice = document.querySelector('#container input:nth-of-type(3)');
-    const ulMoviesParent = document.querySelector('#movies ul');
-    const ulArchiveParent = document.querySelector('#archive ul');
-    const onScreenBtn = document.querySelector('#container button');
-    const clearButton = document.querySelector('#archive button');
-
-    onScreenBtn.addEventListener('click', e => {
-        e.preventDefault();
-        if (name.value === '' || hall.value === '' || ticketPrice.value === '') {
-            return;
-        };
-        if (!Number(ticketPrice.value)) {
-            return;
-        };
-        
-        const li = elements('li');
-        const liSpan = elements('span', '', '', name.value);
-        const liStrong = elements('strong', '', '', `Hall: ${hall.value}`);
-        const liDiv = elements('div');
-        const liDivStrong = elements('strong', '', '', Number(ticketPrice.value).toFixed(2));
-        const liDivInput = elements('input', '', 'Tickets Sold', '');
-        const liDivButton = elements('button', '', '', 'Archive');
-        li.appendChild(liSpan);
-        li.appendChild(liStrong);
-        li.appendChild(liDiv);
-        liDiv.appendChild(liDivStrong);
-        liDiv.appendChild(liDivInput);
-        liDiv.appendChild(liDivButton);
-        ulMoviesParent.appendChild(li);
-        name.value = '';
-        hall.value = '';
-        ticketPrice.value = '';
-
-        liDivButton.addEventListener('click', ev => {
-            ev.preventDefault();
-
-            if (!Number(liDivInput.value)) {
-                return;
-            };
-            const sum = Number(liDivInput.value) * Number(liDivStrong.textContent);
-            const archiveLi = elements('li');
-            const archiveLiSpan = elements('span', '', '', liSpan.textContent);
-            const archiveLiStrong = elements('strong', '', '', `Total amount: ${sum.toFixed(2)}`);
-            const archiveLiDivButton = elements('button', '', '', 'Delete');
-            archiveLi.appendChild(archiveLiSpan);
-            archiveLi.appendChild(archiveLiStrong);
-            archiveLi.appendChild(archiveLiDivButton);
-            ulArchiveParent.appendChild(archiveLi);
-            ev.target.parentElement.parentElement.remove();
-
-            archiveLiDivButton.addEventListener('click', deleteRow => {
-                deleteRow.preventDefault();
-                deleteRow.target.parentElement.remove();
-            });
-            clearButton.addEventListener('click', r => {
-                r.preventDefault();
-                const childEl = r.target.parentElement.children[1].children;
-                for (const el of childEl) {
-                    el.remove();
-                    }
-                });
+    
+        const name = document.querySelector('#container input:nth-of-type(1)');
+        const hall = document.querySelector('#container input:nth-of-type(2)');
+        const ticketPrice = document.querySelector('#container input:nth-of-type(3)');
+        const ulMoviesParent = document.querySelector('#movies ul');
+        const ulArchiveParent = document.querySelector('#archive ul');
+        const clearButton = ulArchiveParent.parentElement.querySelector('button');
+        clearButton.addEventListener('click', () => {
+            ulArchiveParent.innerHTML = '';
         });
-    });
-
-    function elements(type, className, classType, text) {
-        const element = document.createElement(type);
         
-        if (text) {
+        const onScreenBtn = document.querySelector('#container button');
+        onScreenBtn.addEventListener('click', addScene);
+                
+                function addScene(event) {
+                    event.preventDefault();
+                    if (name.value !== '' && hall.value !== '' && ticketPrice.value !== '' && !isNaN(Number(ticketPrice.value))) {
+                        const li = elements('li');
+                        const liSpan = elements('span', '', '', name.value);
+                        const liStrong = elements('strong', '', '', `Hall: ${hall.value}`);
+                        const liDiv = elements('div');
+                        const liDivStrong = elements('strong', '', '', Number(ticketPrice.value).toFixed(2));
+                        const liDivInput = elements('input', '', 'Tickets Sold', '');
+                        const liDivButton = elements('button', '', '', 'Archive');
+                        li.appendChild(liSpan);
+                        li.appendChild(liStrong);
+                        li.appendChild(liDiv);
+                        liDiv.appendChild(liDivStrong);
+                        liDiv.appendChild(liDivInput);
+                        liDiv.appendChild(liDivButton);
+                        ulMoviesParent.appendChild(li);
+                        name.value = '';
+                        hall.value = '';
+                        ticketPrice.value = '';
+                        const button = li.querySelector('div button');
+                        button.addEventListener('click', addArchive);
+                    }
+                }
+
+                function addArchive(e) {
+                    const inputVal = e.target.parentElement.querySelector('input');
+                    const ticket = e.target.parentElement.querySelector('strong');
+                    const movie = e.target.parentElement.parentElement.querySelector('span');
+                    
+                    if (inputVal.value !== '' && !isNaN(Number(inputVal.value))) {
+                        const sum = Number(inputVal.value) * Number(ticket.textContent);
+                        const archiveLi = elements('li');
+                        const archiveLiSpan = elements('span', '', '', movie.textContent);
+                        const archiveLiStrong = elements('strong', '', '', `Total amount: ${sum.toFixed(2)}`);
+                        const archiveLiDivButton = elements('button', '', '', 'Delete');
+                        archiveLi.appendChild(archiveLiSpan);
+                        archiveLi.appendChild(archiveLiStrong);
+                        archiveLi.appendChild(archiveLiDivButton);
+                        ulArchiveParent.appendChild(archiveLi);
+                        
+                        const button = ulArchiveParent.querySelector('button');
+                        button.addEventListener('click', onDelete);
+                    };
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                function onDelete(e) {
+                    e.target.parentElement.remove();
+                }
+    
+        function elements(type, className, classType, text) {
+            const element = document.createElement(type);
             element.textContent = text;
+            if (className) {
+                element.setAttribute('type', className);
+            };
+            if (classType) {
+                element.setAttribute('placeholder', classType);
+            }
+            return element;
         }
-        if (className) {
-            element.setAttribute('type', className);
-        };
-        if (classType) {
-            element.setAttribute('placeholder', classType);
-        }
-        return element;
-    }
 }
