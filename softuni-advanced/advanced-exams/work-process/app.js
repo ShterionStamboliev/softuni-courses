eeb2ce6ac309e76fae031d96575ebbdc614c3834
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-19 21:38:19 +0300

    Added final exam task.

--- softuni-advanced/advanced-exams/work-process/app.js
+++ softuni-advanced/advanced-exams/work-process/app.js
@@ -1,71 +1,71 @@
 function solve() {
-    const form = document.querySelectorAll('input');
-    const hireWorkerButton = document.getElementById('add-worker');
-    const tBodyField = document.getElementById('tbody');
-    const sumId = document.getElementById('sum');
+  const form = document.querySelectorAll("input");
+  const hireWorkerButton = document.getElementById("add-worker");
+  const tBodyField = document.getElementById("tbody");
+  const sumId = document.getElementById("sum");
 
-    hireWorkerButton.addEventListener('click', e => {
-        e.preventDefault();
-        
-        if ([...form].forEach(input => input.value.trim() === '')) {
-            return;
-        };
-        
-            const obj = {}; 
-            [...form].forEach(x => {
-                obj[x.id] = x.value;
-            });
+  hireWorkerButton.addEventListener("click", (e) => {
+    e.preventDefault();
 
-        const tr = elements('tr');
-        elements('td', obj.fname, '', tr);
-        elements('td', obj.lname, '', tr);
-        elements('td', obj.email, '', tr);
-        elements('td', obj.birth, '', tr);
-        elements('td', obj.position, '', tr);
-        elements('td', obj.salary, '', tr);
+    if ([...form].forEach((input) => input.value.trim() === "")) {
+      return;
+    }
 
-        const td = elements('td', '', '', tr);
-        const tdFiredButton = elements('button', 'Fired', 'fired', td);
-        const tdEditButton = elements('button', 'Edit', 'edit', td);
-        tBodyField.appendChild(tr);
-
-        const salary = Number(sumId.textContent);
-        sumId.textContent = (Number(obj.salary) + salary).toFixed(2);
-        [...form].forEach(x => x.value = '');
-        
-        tdEditButton.addEventListener('click', event => {
-            event.preventDefault();
-            
-            [...form].forEach(input => {
-                input.value = obj[input.id];
-            });
-            workerSalary(obj.salary);
-            event.target.parentElement.parentElement.remove();
-        });
-
-        tdFiredButton.addEventListener('click', ev => {
-            ev.preventDefault();
-            ev.target.parentElement.parentElement.remove();
-            workerSalary(obj.salary);
-        });
+    const obj = {};
+    [...form].forEach((x) => {
+      obj[x.id] = x.value;
     });
 
-    function elements(type, text, className, parentEl) {
-        const element = document.createElement(type);
-        element.textContent = text;
+    const tr = elements("tr");
+    elements("td", obj.fname, "", tr);
+    elements("td", obj.lname, "", tr);
+    elements("td", obj.email, "", tr);
+    elements("td", obj.birth, "", tr);
+    elements("td", obj.position, "", tr);
+    elements("td", obj.salary, "", tr);
 
-        if (className) {
-            element.setAttribute('class', className);
-        };
-        if (parentEl) {
-            parentEl.appendChild(element);
-        };
-        return element;
-    }
+    const td = elements("td", "", "", tr);
+    const tdFiredButton = elements("button", "Fired", "fired", td);
+    const tdEditButton = elements("button", "Edit", "edit", td);
+    tBodyField.appendChild(tr);
 
-    function workerSalary(sum) {
-        const salary = Number(sumId.textContent);
-        sumId.textContent = Math.abs((Number(sum) - salary)).toFixed(2);
+    const salary = Number(sumId.textContent);
+    sumId.textContent = (Number(obj.salary) + salary).toFixed(2);
+    [...form].forEach((x) => (x.value = ""));
+
+    tdEditButton.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      [...form].forEach((input) => {
+        input.value = obj[input.id];
+      });
+      workerSalary(obj.salary);
+      event.target.parentElement.parentElement.remove();
+    });
+
+    tdFiredButton.addEventListener("click", (ev) => {
+      ev.preventDefault();
+      ev.target.parentElement.parentElement.remove();
+      workerSalary(obj.salary);
+    });
+  });
+
+  function elements(type, text, className, parentEl) {
+    const element = document.createElement(type);
+    element.textContent = text;
+
+    if (className) {
+      element.setAttribute("class", className);
     }
+    if (parentEl) {
+      parentEl.appendChild(element);
+    }
+    return element;
+  }
+
+  function workerSalary(sum) {
+    const salary = Number(sumId.textContent);
+    sumId.textContent = Math.abs(Number(sum) - salary).toFixed(2);
+  }
 }
-solve()
+solve();
