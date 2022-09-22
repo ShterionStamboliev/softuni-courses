d2248fe79ef2e141243907834a4e61a6f39e2f9c
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-09-22 15:15:41 +0300

    .

---
+++ softuni-advanced/classes-and-attributes-Exercise/05.lengthLimit.js
@@ -0,0 +1,22 @@
+class Stringer {
+    constructor(string, length) {
+        this.innerString = string;
+        this.innerLength = length;
+    }
+    increase(len) {
+        this.innerLength += len;
+    };
+
+    decrease(len) {
+        this.innerLength - len < 0 ? this.innerLength = 0 : this.innerLength-= len;
+    };
+
+    toString() {
+        if (this.innerString.length > this.innerLength) {
+            return this.innerString.substring(0, this.innerLength) + '...';
+        } else {
+            return this.innerString;
+        }
+    };
+}
+
