40a9a2dea0e069624168d297046de78700a2a63a
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-10-03 14:54:33 +0300

    .

---
+++ softuni-advanced/unit-testing-and-error-handling-Lab/rgb/rgbHex.test.js
@@ -0,0 +1,37 @@
+const { expect } = require('chai');
+const { rgbToHexColor } = require('./rgbHex');
+
+describe('rgb hex', () => {
+    it('red is valid', () => {
+        expect(rgbToHexColor(255, 0, 0)).to.equal('#FF0000');
+    });
+
+    it('blue is valid', () => {
+        expect(rgbToHexColor(0, 0, 255)).to.equal('#0000FF');
+    });
+
+    it('green is valid', () => {
+        expect(rgbToHexColor(0, 255, 0)).to.equal('#00FF00');
+    });
+    
+    it('is out of boundary', () => {
+        expect(rgbToHexColor(-256, 255, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, -256, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, -256)).to.be.undefined;
+        expect(rgbToHexColor(256, 255, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 256, 0)).to.be.undefined;
+        expect(rgbToHexColor(0, 0, 256)).to.be.undefined;
+    });
+
+    it('contains floating int', () => {
+        expect(rgbToHexColor(1.2, 123, 5)).to.be.undefined;
+        expect(rgbToHexColor(255, 1.23, 1)).to.be.undefined;
+        expect(rgbToHexColor(255, 123, 1.21)).to.be.undefined;
+    });
+
+    it('contains string parameters', () => {
+        expect(rgbToHexColor(255, 123, 'pesho')).to.be.undefined;
+        expect(rgbToHexColor(255, 'pesho', '1')).to.be.undefined;
+        expect(rgbToHexColor('pesho', 123, '2')).to.be.undefined;
+    });
+});
---
+++ softuni-advanced/unit-testing-and-error-handling-Lab/symmetry/checkForSymmetry.test.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const { isSymmetric } = require('./checkForSymmetry');
+
+describe('arr to be symmetric', () => {
+    it('is symmetric ', () => {
+        expect(isSymmetric([1, 2, 2, 1])).to.be.true;
+    });
+    it('return false if arr is not symmetric', () => {
+        expect(isSymmetric([1, 3, 5])).to.be.false;
+    });
+
+    it('return false for string input', () => {
+        expect(isSymmetric('qwe')).to.be.false;
+    });
+
+    it('return true if input elements are odd number', () => {
+        expect(isSymmetric([1, 100, 1])).to.be.true;
+    });
+
+    it('returns true for empty arrays', () => {
+        expect(isSymmetric([])).to.be.true;
+    });
+
+    it('return false if inputs doesnt match', () => {
+        expect(isSymmetric([1, 100, '1000'])).to.be.false;
+    });
+});
+
