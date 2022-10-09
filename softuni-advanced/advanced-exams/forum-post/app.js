5f6a5ae86147202a3fca2dba5754c57e2ee95758
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-10-09 13:51:04 +0300

    Updated forum posts.

--- softuni-advanced/advanced-exams/forum-post/app.js
+++ softuni-advanced/advanced-exams/forum-post/app.js
@@ -1,154 +1,66 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    const postTitleInput = document.getElementById('post-title');
-    const postCategoryInput = document.getElementById('post-category');
-    const textArea = document.getElementById('post-content');
-    const reviewListParent = document.getElementById('review-list');
-    const publishedListParent = document.getElementById('published-list');
-    const clearButton = document.getElementById('clear-btn');
+    const inputs = {
+      inputTitle: document.getElementById('post-title'),
+      inputCategory: document.getElementById('post-category'),
+      inputContent: document.getElementById('post-content')
+    };
 
+    const lists = {
+      review: document.getElementById('review-list'),
+      publish: document.getElementById('published-list')
+    }
+    const clearBtn = document.getElementById('clear-btn');
+    
     document.getElementById('publish-btn').addEventListener('click', e => {
       e.preventDefault();
-      if (postTitleInput.value === '' || postCategoryInput.value === '' || textArea.value === '') return;
+      const title = inputs.inputTitle.value;
+      const category = inputs.inputCategory.value;
+      const content = inputs.inputContent.value;
+      if (title === '' || category === '' || content === '') return;
 
       const li = elements('li', 'rpost');
-      const article = elements('article', '', '');
-      const h4 = elements('h4', '', postTitleInput.value);
-      const p1 = elements('p', '', `Category: ${postCategoryInput.value}`);
-      const p2 = elements('p', '', `Content: ${textArea.value}`);
-      const buttonApprove = elements('button', 'action-btn approve', 'Approve');
-      const buttonEdit = elements('button', 'action-btn edit', 'Edit');
-      article.appendChild(h4);
-      article.appendChild(p1);
-      article.appendChild(p2);
-      li.appendChild(article);
-      li.appendChild(buttonApprove);
-      li.appendChild(buttonEdit);
-      reviewListParent.appendChild(li);
+      const article = elements('article', '', '', li);
+      elements('h4', '', title, article);
+      elements('p', '', `Category: ${category}`, article);
+      elements('p', '', `Content: ${content}`, article);
+      const buttonApprove = elements('button', 'action-btn approve', 'Approve', li);
+      const buttonEdit = elements('button', 'action-btn edit', 'Edit', li);
+      lists.review.appendChild(li);
 
-      postTitleInput.value = '';
-      postCategoryInput.value = '';
-      textArea.value = '';
+      inputs.inputTitle.value = '';
+      inputs.inputCategory.value = '';
+      inputs.inputContent.value = '';
 
-      buttonEdit.addEventListener('click', ev => {
-        postTitleInput.value = h4.textContent;
-        postCategoryInput.value = p1.textContent;
-        textArea.value = p2.textContent;
-        ev.target.parentElement.remove();
+      buttonEdit.addEventListener('click', () => {
+        inputs.inputTitle.value = title;
+        inputs.inputCategory.value = category;
+        inputs.inputContent.value = content;
+        li.remove();
       });
 
-      buttonApprove.addEventListener('click', event => {
-        const _li = elements('li', 'rpost');
-        const _article = elements('article', '', '');
-        const _h4 = elements('h4', '', h4.textContent);
-        const _p1 = elements('p', '', p1.textContent);
-        const _p2 = elements('p', '', p2.textContent);
-        _article.appendChild(_h4);
-        _article.appendChild(_p1);
-        _article.appendChild(_p2);
-        _li.appendChild(_article);
-        event.target.parentElement.remove();
-        publishedListParent.appendChild(_li);
-        const removeButtons = li.querySelectorAll('button');
-        for (const button of removeButtons) {
-          button.remove();
-        }
+      buttonApprove.addEventListener('click', () => {
+        lists.publish.appendChild(li);
+        buttonApprove.remove();
+        buttonEdit.remove();
       });
-      clearButton.addEventListener('click', () => {
-       const removeLi = publishedListParent.querySelectorAll('li');
-        for (el of removeLi) {
-          el.remove();
-        }
+
+      clearBtn.addEventListener('click', () => {
+        lists.publish.innerHTML = '';
       });
     });
 
-    function elements(type, className, content) {
+    function elements(type, className, content, parent) {
       const element = document.createElement(type);
       element.textContent = content;
 
+      if (parent) {
+        parent.appendChild(element);
+      };
       if (className) {
         element.setAttribute("class", className);
-      }
+      };
       return element;
     }
 }
-
-
-/* 
-function solve() {
-    const postTitleInput = document.getElementById('post-title');
-    const postCategoryInput = document.getElementById('post-category');
-    const textArea = document.getElementById('post-content');
-    const reviewListParent = document.getElementById('review-list');
-    const publishedListParent = document.getElementById('published-list');
-    const clearButton = document.getElementById('clear-btn');
-
-    document.getElementById('publish-btn').addEventListener('click', e => {
-      e.preventDefault();
-      if (postTitleInput.value === '' || postCategoryInput.value === '' || textArea.value === '') return;
-
-      const li = elements('li', 'rpost');
-      const article = elements('article', '', '');
-      const h4 = elements('h4', '', postTitleInput.value);
-      const p1 = elements('p', '', postCategoryInput.value);
-      const p2 = elements('p', '', textArea.value);
-      const buttonApprove = elements('button', 'action-btn approve', 'Approve');
-      const buttonEdit = elements('button', 'action-btn edit', 'Edit');
-      article.appendChild(h4);
-      article.appendChild(p1);
-      article.appendChild(p2);
-      li.appendChild(article);
-      li.appendChild(buttonApprove);
-      li.appendChild(buttonEdit);
-      reviewListParent.appendChild(li);
-
-      postTitleInput.value = '';
-      postCategoryInput.value = '';
-      textArea.value = '';
-
-      buttonEdit.addEventListener('click', ev => {
-        postTitleInput.value = h4.textContent;
-        postCategoryInput.value = p1.textContent;
-        textArea.value = p2.textContent;
-        ev.target.parentElement.remove();
-      });
-
-      buttonApprove.addEventListener('click', event => {
-        const _li = elements('li', 'rpost');
-        const _article = elements('article', '','');
-        const _h4 = elements('h4', '', h4.textContent);
-        const _p1 = elements('p', '', p1.textContent);
-        const _p2 = elements('p', '', p2.textContent);
-        _article.appendChild(_h4);
-        _article.appendChild(_p1);
-        _article.appendChild(_p2);
-        _li.appendChild(_article);
-        publishedListParent.appendChild(_li);
-        const removeButtons = li.querySelectorAll('button');
-        for (const button of removeButtons) {
-          button.remove();
-        }
-        event.target.parentElement.remove();
-      });
-    });
-
-      clearButton.addEventListener('click', () => {
-       const removeLi = publishedListParent.querySelectorAll('li');
-        for (el of removeLi) {
-          el.remove();
-        }
-      });
-
-
-    function elements(type, className, content) {
-      const element = document.createElement(type);
-      element.textContent = content;
-
-      if (className) {
-        element.setAttribute("class", className);
-      }
-      return element;
-    }
-}
-*/
