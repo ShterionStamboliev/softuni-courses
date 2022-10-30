f54b2ffd83d0aa3b6625fd2287ab1310d4c9f1c2
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-10-30 12:20:07 +0200

    .

--- softuni-advanced-js-applications/Lab/03.Gighub-Commits/app.js
+++ softuni-advanced-js-applications/Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,22 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+  const username = document.getElementById("username").value;
+  const repo = document.getElementById("repo").value;
+  const commitsParent = document.getElementById("commits");
+
+  try {
+    const result = await fetch(
+      `https://api.github.com/repos/${username}/${repo}/commits`
+    );
+    if (result.ok === false) {
+      throw new Error(`${result.status} ${result.statusText}`);
+    }
+    const data = await result.json();
+    for (const { commit } of data) {
+      const li = document.createElement("li");
+      li.textContent = `${commit.author.name}: ${commit.message}`;
+      commitsParent.appendChild(li);
+    }
+  } catch (err) {
+    commitsParent.innerHTML = `Error: ${err.message}`;
+  }
 }
