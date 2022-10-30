60aafdfb70638983e565c0da0b13f3c6a3e6be43
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-10-30 13:49:34 +0200

    .

--- softuni-advanced-js-applications/Exercises/01.Bus-Stop/app.js
+++ softuni-advanced-js-applications/Exercises/01.Bus-Stop/app.js
@@ -1,3 +1,21 @@
-function getInfo() {
-    
+async function getInfo() {
+  const stopId = document.getElementById("stopId").value;
+  const busesParent = document.getElementById("buses");
+  const busStop = document.getElementById("stopName");
+
+  try {
+    const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
+    const data = await response.json();
+    busesParent.textContent = "";
+    busStop.textContent = data.name;
+
+    for (const [id, time] of Object.entries(data.buses)) {
+      const li = document.createElement("li");
+      li.textContent = `Bus ${id} arrives in ${time} minutes`;
+      busesParent.appendChild(li);
+    }
+  } catch (err) {
+    busesParent.textContent = "";
+    busStop.textContent = "Error";
+  }
 }
