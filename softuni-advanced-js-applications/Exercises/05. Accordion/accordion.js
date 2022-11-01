84f305a1e436b4935d74bb9223ba44ab6adfcb82
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-11-01 21:14:22 +0200

    .

--- softuni-advanced-js-applications/Exercises/05. Accordion/accordion.js
+++ softuni-advanced-js-applications/Exercises/05. Accordion/accordion.js
@@ -1,10 +1,9 @@
 async function solution() {
-    const main = document.getElementById('main');
     const response = await fetch('http://localhost:3030/jsonstore/advanced/articles/list');
     const data = await response.json();
     
     Object.values(data).forEach(({ _id, title }) => {
-        const divAccordion = elements('div', '', 'accordion', '', main);
+        const divAccordion = elements('div', '', 'accordion', '', document.getElementById('main'));
         const divHead = elements('div', '', 'head', '', divAccordion);
         const divExtra = elements('div', '', 'extra', '', divAccordion);
         divExtra.style.display = 'none';
@@ -12,7 +11,7 @@ async function solution() {
         elements('button', 'More', 'button', _id, divHead);
         elements('p', '', '', '', divExtra);
     });
-    const allBtns = [...main.querySelectorAll('button')];
+    const allBtns = [...document.getElementById('main').querySelectorAll('button')];
     allBtns.forEach(b => {
         b.addEventListener('click', async (e) => {
             const response = await fetch('http://localhost:3030/jsonstore/advanced/articles/details/' + b.id);
@@ -21,7 +20,7 @@ async function solution() {
             if (e.target.textContent === 'More') {
                 e.target.textContent = 'Less';
                 e.target.parentElement.parentElement.children[1].style.display = 'block';
-            } else if (e.target.textContent === 'Less') {
+            } else {
                 e.target.textContent = 'More';
                 e.target.parentElement.parentElement.children[1].style.display = 'none';
             }
@@ -30,18 +29,10 @@ async function solution() {
 }
 function elements(type, text, className, ids, parent) {
     const el = document.createElement(type);
-    if (text) {
-        el.textContent = text;
-    }
-    if (className) {
-        el.setAttribute('class', className);
-    }
-    if (parent) {
-        parent.appendChild(el);
-    }
-    if (ids) {
-        el.id = ids;
-    }
-    return el;
+        text ? el.textContent = text : '';
+        className ? el.setAttribute('class', className) : '';
+        parent ? parent.appendChild(el) : '';
+        ids ? el.id = ids : '';
+        return el;
 };
 solution();
