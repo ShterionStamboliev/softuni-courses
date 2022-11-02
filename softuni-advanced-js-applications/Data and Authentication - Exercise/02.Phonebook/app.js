ca1b6c101cc90a5780dd2f67107b1aa3a640dd16
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-11-02 23:50:12 +0200

    .

--- softuni-advanced-js-applications/Data and Authentication - Exercise/02.Phonebook/app.js
+++ softuni-advanced-js-applications/Data and Authentication - Exercise/02.Phonebook/app.js
@@ -1,5 +1,42 @@
-function attachEvents() {
-    console.log('TODO...');
+async function attachEvents() {
+    const phonebook = document.getElementById('phonebook');
+    
+    document.getElementById('btnLoad').addEventListener('click', onLoad);
+    document.getElementById('btnCreate').addEventListener('click', async () => {
+        const personName = document.getElementById('person');
+        const personPhone = document.getElementById('phone');
+        if (!personName.value || !personPhone.value) return;
+        await fetch('http://localhost:3030/jsonstore/phonebook', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({ person: personName.value, phone: personPhone.value })
+        });
+        personName.value = '';
+        personPhone.value = '';
+        onLoad();
+    });
+    async function onLoad() {
+        const response = await fetch('http://localhost:3030/jsonstore/phonebook');
+        const data = await response.json();
+        phonebook.replaceChildren();
+        Object.values(data).forEach(d => {
+            const li = elements('li', `${d.person}: ${d.phone}`, phonebook);
+            const deleteBtn = elements('button', 'Delete', li);
+            deleteBtn.addEventListener('click', async e => {
+                await fetch('http://localhost:3030/jsonstore/phonebook/' + d._id, {
+                    method: 'DELETE',
+                });
+                e.target.parentElement.remove();
+            });
+        });
+    };
+    function elements(type, text, parent) {
+        const el = document.createElement(type);
+        text ? el.textContent = text : '';
+        parent ? parent.appendChild(el) : '';
+        return el;
+    }
 }
-
 attachEvents();
