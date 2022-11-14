48dc1b0b6ad609487244fed425869073191203d2
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-11-14 17:15:48 +0200

    .

--- softuni-advanced-js-applications/Client Side Rendering - Exercise/03.Search-in-List/search.js
+++ softuni-advanced-js-applications/Client Side Rendering - Exercise/03.Search-in-List/search.js
@@ -1,3 +1,31 @@
+import { towns } from "./towns.js";
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+const div = document.getElementById('towns');
+
+const templ = html`
+<ul>
+ ${towns.map(t => {
+    return html`<li id=${t}>${t}</li>`
+    })}
+</ul>`;
+
+render(templ, div);
+document.querySelector('button').addEventListener('click', search);
+
 function search() {
-   // TODO
+    const result = document.getElementById('result');
+    let matches = 0;
+    const field = document.getElementById('searchText').value
+    for (const t of towns) {
+        if (t.toLowerCase().includes(field.toLowerCase())) {
+            const el = document.getElementById(`${t}`);
+            el.setAttribute('class', 'active');
+            matches++;
+        } else {
+            const el = document.getElementById(`${t}`);
+            el.removeAttribute('class', 'active')
+        }
+    }
+    result.textContent = `${matches} matches found`;
 }
