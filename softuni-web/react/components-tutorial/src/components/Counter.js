96f6110b3a023192121d414c393adb65bf304062
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-03-01 17:24:14 +0200

    .

--- softuni-web/react/components-tutorial/src/components/Counter.js
+++ softuni-web/react/components-tutorial/src/components/Counter.js
@@ -20,11 +20,11 @@ function Counter() {
         <div>
             <p>You clicked {count} times</p>
 
-            <button style={count > 0 ? setStyleBlack() : setStyleGreen()} onClick={() => incrementCount()}>Increment count</button>
+            <button style={count > 9 ? setStyleBlack() : setStyleGreen()} onClick={() => incrementCount()}>Increment count</button>
 
             <button style={count > 0 ? setStyleRed() : setStyleBlack()} onClick={() => decrementCount()}>Decrement count</button>
 
-            <button style={count > 9 ? setResetStyle() : setStyleBlack()} onClick={() => resetCount()}>Reset count</button>
+            <button style={count > 0 ? setResetStyle() : setStyleBlack()} onClick={() => resetCount()}>Reset count</button>
         </div>
     );
 };
